5a20f81a0438b533e1e5b605e9e474841965fb3d
NagnathSomanwar <nagnathsomanwar@gmail.com>
2024-10-05 16:38:42 +0530

    solved problem 2

---
+++ database.py
@@ -0,0 +1,24 @@
+restaurant_data = {
+    "restaurant_1": {
+        "burger": 4,
+
+    },
+    "restaurant_2": {
+        "tofu_log": 8,
+        "burger": 5,
+
+    },
+    "restaurant_3": {
+        "tofu_log": 6.5,
+        "chef_salad": 3
+    },
+
+    "restaurant_4": {
+        "chef_salad": 6.5,
+        "steak_salad_sandwich": 3
+    },
+   "restaurant_5": {
+        "chef_salad": 6.5,
+        "steak_salad_sandwich": 3
+    }
+}
---
+++ problem2.py
@@ -0,0 +1,25 @@
+from database import *
+
+order_items = ["tofu_log", "burger"]
+
+
+def find_min_price(order_items):
+    price = float('inf')
+    hotel_with_min_prices = None
+    found = False
+
+    # checking for the allItems in a hotel
+    for restaurant, menu in restaurant_data.items():
+        if all(food in menu for food in order_items):
+            total_price = sum(menu[food] for food in order_items)
+
+            if total_price < price:
+                price = total_price
+                hotel_with_min_prices = restaurant
+                found = True
+
+
+    return (hotel_with_min_prices, price) if found else None
+
+
+result = find_min_price(order_items)
